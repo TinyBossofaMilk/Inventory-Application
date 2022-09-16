fd934bdbfdddf5374530f1f9a18ce1575ac2fe43
TinyBossofaMilk <ppcctan@gmail.com>
2022-09-16 01:35:43 -0700

    added types_list controller code

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -5,5 +5,17 @@ const { body, validationResult } = require("express-validator");
 var async = require('async');
 
 exports.types_list = function(req, res) {
-    res.render('type-list');
+
+    async.parallel({
+        type: function(callback) {
+            Type.find();
+        }
+    }, function (err, results) {
+        if (err) {return next(err);}
+        res.render('type-list');
+
+    }
+    )
+
+
 };
